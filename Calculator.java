c5aebb88def07792ee8b329982b6692e5db735bb
Joni Pennala <joni.pennala@gmail.com>
2017-10-26 20:20:23 +0300

    Refactor code

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,47 @@
+import java.util.Scanner;
+
 public class Calculator {
-	public int evaluate(String expression){
-		int sum = 0;
-		for (String summand: expression.split("\\+"))
-			sum += Integer.valueOf(summand);
-		return sum;
-	}
+
+    public static int sumCalc (int firstVal, int secondVal) {
+		return firstVal + secondVal;
+    }
+	
+	public static int subCalc (int firstVal, int secondVal) {
+		return firstVal - secondVal;
+    }
+	
+    public static int mulCalc (int firstVal, int secondVal) {
+		return firstVal * secondVal;
+    }
+	
+	public static int divCalc (int firstVal, int secondVal) {
+		return firstVal / secondVal;
+    }
+
+    public static void main (String[] args) {
+		int firstValue;
+		int secondValue;
+		String calculationMethod;
+		Scanner reader = new Scanner(System.in);
+		System.out.println ("Insert first value: ");
+		firstValue = reader.nextInt();
+		System.out.println ("You entered: " + firstValue);
+		System.out.println ("Insert second value: ");
+		secondValue = reader.nextInt();
+		System.out.println ("You entered: " + secondValue);
+		System.out.println ("Insert '+', '-', '*' or '/' to calculate your values: ");
+		calculationMethod = reader.next();
+		System.out.println ("Calculation method is: " + calculationMethod);
+		int result = 0;
+		if(calculationMethod.equals("+")) {
+			result = sumCalc(firstValue, secondValue);
+		} else if (calculationMethod.equals("-")) {
+			result = subCalc(firstValue, secondValue);
+		} else if (calculationMethod.equals("*")) {
+			result = mulCalc(firstValue, secondValue);
+		} else if (calculationMethod.equals("/")) {
+			result = divCalc(firstValue, secondValue);
+		}
+		System.out.println ("Result is: " + result);
+    }
 }
--- CalculatorTest.java
+++ CalculatorTest.java
@@ -3,9 +3,31 @@ import org.junit.Test;
 
 public class CalculatorTest {
 	@Test
-	public void evaluatesExpression(){
+	public void testAddition(){
 		Calculator calculator = new Calculator();
-		int sum = calculator.evaluate("1+2+3");
-		assertEquals(6, sum);
+		int sum = calculator.sumCalc(2, 3);
+		assertEquals(5, sum);
 	}
+	
+	@Test
+	public void testSubtraction(){
+		Calculator calculator = new Calculator();
+		int sub = calculator.subCalc(1, 3);
+		assertEquals(-2, sub);
+	}
+	
+	@Test
+	public void testMultiplication(){
+		Calculator calculator = new Calculator();
+		int mul = calculator.mulCalc(4, 5);
+		assertEquals(20, mul);
+	}
+	
+	@Test
+	public void testDivision(){
+		Calculator calculator = new Calculator();
+		int div = calculator.divCalc(10, 2);
+		assertEquals(5, div);
+	}
+	
 }
